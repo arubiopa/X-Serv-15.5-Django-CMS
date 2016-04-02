9ec9e6a0d9bd34dc7814c4972b4d1b657b006e26
arubiopa <anggylotis@gmail.com>
2016-04-02 17:09:00 +0200

    ejercicio resuelto

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from models import Pages
 # Create your views here.
+def cms(request, recurso):
+    try:
+		contenido = Pages.objects.get(name=recurso)
+		return HttpResponse(contenido.name+ ':' + contenido.page)
+	except Pages.DoesNotExist:
+		return HttpResponse("Recurso no encontrado: " + recurso)
